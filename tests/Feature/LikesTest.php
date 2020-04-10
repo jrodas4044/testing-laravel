a9b7abf44dacd439f9da075d3f8c957552142cc3
Jonhathan Rodas <jrodas4044@gmail.com>
2020-04-09 19:11:14 -0600

    Test a Fluent API with TDD

--- tests/Feature/LikesTest.php
+++ tests/Feature/LikesTest.php
@@ -13,69 +13,57 @@ class LikesTest extends TestCase
 {
     use DatabaseMigrations;
 
+    protected $post;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->post = factory(Post::class)->create();
+        $this->signIn();
+    }
+
     public function test_it_a_can_like_a_post()
     {
-        $post = factory(Post::class)->create();
-        $user = factory(User::class)->create();
-
-        $this->actingAs($user);
-
-        $post->like();
+        $this->post->like();
 
         $this->assertDatabaseHas('likes',[
-            'user_id' => $user->id,
-            'likeable_id' => $post->id,
-            'likeable_type' => get_class($post)
+            'user_id' => $this->user->id,
+            'likeable_id' => $this->post->id,
+            'likeable_type' => get_class($this->post)
         ]);
 
-        $this->assertTrue($post->isLiked());
+        $this->assertTrue($this->post->isLiked());
     }
 
     public function test_it_a_user_can_unlike_a_post()
     {
-        $post = factory(Post::class)->create();
-        $user = factory(User::class)->create();
-
-        $this->actingAs($user);
-
-        $post->like();
-        $post->unlike();
+        $this->post->like();
+        $this->post->unlike();
 
         $this->assertDatabaseMissing('likes',[
-            'user_id' => $user->id,
-            'likeable_id' => $post->id,
-            'likeable_type' => get_class($post)
+            'user_id' => $this->user->id,
+            'likeable_id' => $this->post->id,
+            'likeable_type' => get_class($this->post)
         ]);
 
-        $this->assertFalse($post->isLiked());
-
+        $this->assertFalse($this->post->isLiked());
     }
 
     public function test_it_a_user_may_toggle_a_posts_like_status()
     {
-        $post = factory(Post::class)->create();
-        $user = factory(User::class)->create();
+        $this->post->toggle();
 
-        $this->actingAs($user);
+        $this->assertTrue($this->post->isLiked());
 
-        $post->toggle();
+        $this->post->toggle();
 
-        $this->assertTrue($post->isLiked());
-
-        $post->toggle();
-
-        $this->assertFalse($post->isLiked());
+        $this->assertFalse($this->post->isLiked());
     }
 
     public function test_it_a_post_knows_many_likes_it_has()
     {
-        $post = factory(Post::class)->create();
-        $user = factory(User::class)->create();
+        $this->post->toggle();
 
-        $this->actingAs($user);
-
-        $post->toggle();
-
-        $this->assertEquals(1 , $post->likesCount);
+        $this->assertEquals(1 , $this->post->likesCount);
     }
 }
